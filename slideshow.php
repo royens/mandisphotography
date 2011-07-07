02c6f1477dc9ca5bcafa2b32f379be58716fc533
Roy Ens <Roy.Ens@gmail.com>
2011-07-07 11:37:10 -0400

    Added slideshow template page.

---
+++ slideshow.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Template Name: Slideshow Page
+ *
+ * One column template for pages that include a
+ * rotating slideshow using JQuery Cycle plugin.
+ * 
+ * @package mandisphotography
+ * @subpackage theme
+ * @version 0.0.1
+ * @since 0.0.1
+ */
+?>
+
+<?php get_header(); ?>
+
+<div id="content" class="one-column" role="main">
+
+    <?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
+        
+        <div id="post-<?php the_ID(); ?>" <?php post_class( 'slideshow' ); ?>>
+            <?php re_get_images( 'slideshow', '0', '0', 'full', "$post->ID", '0', 'attachment-image', 'div', 'small-thumb' ); ?>
+        </div><!-- #post-## -->
+
+    <?php endwhile; ?>
+
+</div><!-- #content .one-column -->
+
+<?php get_footer(); ?>
