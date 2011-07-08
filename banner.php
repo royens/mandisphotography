79757f7a733a65db6f9687a084f4850274cd98f5
Roy Ens <Roy.Ens@gmail.com>
2011-07-08 18:19:47 -0400

    Added new banner page template.

---
+++ banner.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Template Name: Banner Page
+ *
+ * Two column page with a large banner
+ * at the top.
+ *
+ * @package mandisphotography
+ * @subpackage theme
+ * @version 0.0.1
+ * @since 0.0.1
+ */
+?>
+
+<?php get_header(); ?>
+
+<div id="banner-image">
+    <?php re_get_images( 'full', '0', '0', 'full', "$post->ID", '0', 'attachment-banner', '0', 'small-thumb' ); ?>
+</div><!-- #banner-image -->
+
+<div id="content" role="main">
+    
+    <?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
+
+        <div id="post-<?php the_ID(); ?>" <?php post_class( 'banner' ); ?>>
+            <h1 class="entry-title"><?php the_title(); ?></h1>
+
+            <div class="entry-content">
+                <?php the_content(); ?>
+                <?php wp_link_pages( array( 'before' => '<div class="page-links">Pages:', 'after' => '</div>' ) ); ?>
+            </div><!-- .entry-content -->
+        </div><!-- #post-## -->
+
+        <?php comments_template( '', true ); ?>
+
+    <?php endwhile; ?>
+
+</div><!-- #content -->
+
+<?php get_sidebar(); ?>
+<?php get_footer(); ?>
