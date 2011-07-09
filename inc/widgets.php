95e9be66e9da55694ea673f1f4c59178be8b022e
Roy Ens <roy.ens@gmail.com>
2011-07-08 21:26:43 -0400

    Added base file for the Widget class.

---
+++ inc/widgets.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Makes a custom widget for displaying images with links
+ * in any sidebar.
+ *
+ * @package MandisPhotography Theme
+ * @version 0.0.2
+ * @since 0.0.2
+ */
+
+class Mandis_Photography_Image_Widget extends WP_Widget {
+
+    /**
+     * Constructor
+     *
+     * @return void
+     **/
+    function Mandis_Photography_Image_Widget {
+    }
+
+    function widget( $args, $instance ) {
+    }
+
+    function update( $new_instance, $old_instance ) {
+    }
+
+    function form( $instance ) {
+    }
+}
