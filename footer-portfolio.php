1cf7703fc4b19327740a866a0ea8153a8a80ce91
Roy Ens <roy.ens@gmail.com>
2011-07-10 12:52:10 -0400

    Added the new header and footer templates for the portfolio page template.

---
+++ footer-portfolio.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Displays the HTML for the portfolio footer.
+ *
+ * @package mandisphotography
+ * @version 0.0.2
+ * @since 0.0.2
+ */
+?>
+</div><!-- #main-portfolio -->
+
+<?php wp_footer(); ?>
+</body>
+</html>
---
+++ header-portfolio.php
@@ -0,0 +1,68 @@
+<?php
+/**
+ * Theme header for portfolio pages.
+ *
+ * @package mandisphotography
+ * @version 0.0.2
+ * @since 0.0.1
+ */
+?>
+<!doctype html>
+<html <?php language_attributes(); ?>>
+    <head>
+        <meta charset="<?php bloginfo( 'charset' ); ?>" />
+        <title>
+            <?php
+                /*
+                 * Print the <title> tag based on what's being viewed.
+                 */
+                global $page, $paged;
+
+                wp_title( '|', true, 'right' );
+
+                // Add the blog name
+                bloginfo( 'name' );
+
+                // Add the blog description for home/front page.
+                $site_description = get_bloginfo( 'description', 'display' );
+                if ( $site_description && ( is_home() || is_front_page() ) )
+                    echo " | $site_description";
+
+                // Add a page number if necessary
+                if ( $paged >= 2 || $page >= 2 )
+                    echo ' | ' . sprintf( 'Page %s', max( $paged, $page ) );
+            ?>
+        </title>
+
+        <link rel="profile" href="http:/gmpgs.org/xfn/11" />
+        <link rel="stylesheet" type="text/css" media="all" href="<?php bloginfo( 'stylesheet_url' ); ?>" />
+        <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
+
+        <?php 
+            if ( is_singular() && get_option( 'thread_comments' ) )
+                wp_enqueue_script( 'comment-reply' );
+            wp_head();
+        ?>
+    </head>
+
+    <body <?php body_class( 'portfolio' ); ?>>
+
+        <img class="background-left" src="<?php echo get_template_directory_uri(); ?>/images/swirl-left.png" alt="Background Image" />
+        <img class="background-right" src="<?php echo get_template_directory_uri(); ?>/images/swirl-right.png" alt="Background Image" />
+
+        <div id="portfolio-header">
+            <div id="header-wrapper">
+                <div id="branding" role="banner">
+                    <div id="site-title"><a href="<?php echo home_url( '/' ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></div>
+                    <div id="tag-line"><?php bloginfo( 'description' ); ?></div>
+                </div><!-- #branding -->
+            
+                <div id="main-nav" class="nav" role="navigation">
+                    <div class="skip-link screen-reader-text"><a href="#main-portfolio" title="Skip to content">Skip to content</a></div>
+                    <?php wp_nav_menu( 'container_class' => 'menu-header', 'theme_location' => 'primary' ) ); ?>
+                </div><!-- #main-nav -->
+            </div><!-- #header-wrapper -->
+        </div><!-- #header -->
+
+        <div id="main-portfolio">
+
