bea90fa734e5bb704c991fdfecc4c2e8e29b337e
Roy Ens <Roy.Ens@gmail.com>
2011-07-11 12:04:15 -0400

    Moved the infobar outside of the thumbs-wrapper div.

--- footer-portfolio.php
+++ footer-portfolio.php
@@ -7,7 +7,16 @@
  * @since 0.0.2
  */
 ?>
+
 </div><!-- #main-portfolio -->
+<div class="infobar">
+    <div id="info-left">
+        <p><b><?php bloginfo( 'name' ); ?></b> &copy; 2010, 2011 all images.</p>
+    </div><!-- #info-right -->
+    <div id="info-right">
+        <p>Follow us on <a href="<?php echo $facebook_link; ?>" title="Facebook Link"><img src="<?php echo get_template_directory_uri(); ?>/images/f_logo.jpg" width="30" height="30" alt="Facebook Logo" title="Facebook Logo" /></a></p>
+    </div><!-- #info-right -->
+</div><!-- #info-bar -->
 
 <?php wp_footer(); ?>
 </body>
--- portfolio.php
+++ portfolio.php
@@ -24,14 +24,6 @@
     <div id="thumbs-container" style="/*visibility:hidden;*/">
         <?php re_get_images( 'portfolio-thumb', '0', '0', 'full', "$post->ID", '0', 'portfolio-image', '0', '' ); ?>
     </div><!-- #thumbs-container -->
-    <div class="infobar">
-        <div id="info-left">
-            <p><b><?php bloginfo( 'name' ); ?></b> &copy; 2010, 2011 all images.</p>
-        </div><!-- #info-right -->
-        <div id="info-right">
-            <p>Follow us on <a href="<?php echo $facebook_link; ?>" title="Facebook Link"><img src="<?php echo get_template_directory_uri(); ?>/images/f_logo.jpg" width="30" height="30" alt="Facebook Logo" title="Facebook Logo" /></a></p>
-        </div><!-- #info-right -->
-    </div><!-- #info-bar -->
 </div><!-- #thumbs-wrapper -->
 
 <?php get_footer( 'portfolio' ); ?>
