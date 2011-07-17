375137517d11be53ca21f2ced3b81a226dde08d6
Roy Ens <roy.ens@gmail.com>
2011-07-16 21:02:56 -0400

    Added call  to get_fb_link().

--- footer-portfolio.php
+++ footer-portfolio.php
@@ -10,7 +10,7 @@
 
 </div><!-- #main-portfolio -->
 <div class="infobar">
-    <p><b><?php bloginfo( 'name' ); ?></b> &copy; 2010, 2011 all images | Follow us on <a href="<?php echo $facebook_link; ?>" title="Facebook Link"><img src="<?php echo get_template_directory_uri(); ?>/images/f_logo.jpg" width="16" height="16" alt="Facebook Logo" title="Facebook Logo" /></a></p>
+    <p><b><?php bloginfo( 'name' ); ?></b> &copy; 2010, 2011 all images | Follow us on <a href="<?php echo get_fb_link(); ?>" title="Facebook Link"><img src="<?php echo get_template_directory_uri(); ?>/images/f_logo.jpg" width="16" height="16" alt="Facebook Logo" title="Facebook Logo" /></a></p>
 </div><!-- #info-bar -->
 
 <?php wp_footer(); ?>
