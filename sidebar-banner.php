499d5733a63595b87ba2bc94d1f7e2bbf9b8bd98
Roy Ens <roy.ens@gmail.com>
2011-07-18 00:06:25 -0400

    Added new sidebar template for banner pages.

---
+++ sidebar-banner.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Displays the HTML for the sidebar on banner pages.
+ *
+ * @package mandisphotography
+ * @version 0.0.2
+ * @since 0.0.1
+ */
+?>
+
+<div id="sidebar-banner">
+    <ul class="primary">
+        <?php dynamic_sidebar( 'banner-widget-area' ); ?>
+    </ul>
+</div><!-- #sidebar-banner -->
