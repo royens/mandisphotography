f49669b1ed4724a71c468365bdcd0c2e1530b77e
Roy Ens <Roy.Ens@gmail.com>
2011-07-21 02:13:29 -0400

    Removed all debugging output.

--- js/jquery.gallery.js
+++ js/jquery.gallery.js
@@ -181,25 +181,17 @@ jQuery(function() {
         var heightMargin = 0;
         var winHeight = jQuery(window).height();
         var winWidth = jQuery(window).width();
-        console.log( 'Width ' + winWidth + ' Height ' + winHeight );
 
-        if(mode == 'expanded' && winHeight <= 800) {
-            console.log( 'inside height <= 800' );
+        if(mode == 'expanded' && winHeight <= 800)
             heightMargin = 62;
-        }
-        else if(mode == 'expanded' && winHeight > 800) {
-            console.log( 'inside height > 800' );
+        else if(mode == 'expanded' && winHeight > 800)
             heightMargin = 180;
-        }
-        else if(mode == 'small') {
-            console.log( 'inside mode is small' );
+        else if(mode == 'small')
             heightMargin = 220;
-            }
 
         //type 1 is animate type 0 is normal
         var windowH = winHeight-heightMargin;
         var windowW = winWidth-widthMargin;
-        console.log( 'Changed Width ' + windowW + ' Changed Height ' + windowH );
         var theImage = new Image();
         theImage.src = $image.attr("src");
         var imgwidth = theImage.width;
